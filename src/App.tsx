4ac6053afa1d031b0bad4fa0efe190dc4de39590
Adithya V <adithyavinod1943@gmail.com>
2023-06-30 15:19:24 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
-
-
+import {Routes,Route} from "react-router-dom"
+import { Container } from 'react-bootstrap'
+import { Home } from './pages/Home'
+import { Store } from './pages/Store'
+import { About } from './pages/About'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello TS</h1>
-    </>
+   <Container>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/store' element={<Store/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
+   </Container>
   )
 }
 
