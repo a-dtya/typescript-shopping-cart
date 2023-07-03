0c2e043770a6e69e03f28c6ca923428ff2a3b0ab
Adithya V <adithyavinod1943@gmail.com>
2023-07-03 12:18:38 +0530

    Modified CartItem(name,qty,price,ttl price,remove)

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
-import { Stack } from "react-bootstrap"
+import { Stack,Button } from "react-bootstrap"
 import { useShoppingCart } from "../context/ShoppingCartContext"
 import storeItem from "../data/items.json"
+import { formatCurrency } from "../utilities/formatCurrency"
 type CartItemProps = {
     id: number
     quantity: number
@@ -15,6 +16,26 @@ export function CartItem({id,quantity}:CartItemProps){
     return(
         <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
             <img src={item.image} style={{width:'125px',height:"75px",objectFit:"cover"}}></img>
+            <div className="me-auto">
+                <div>
+                    {item.title} {" "}{
+                        <span className="text-muted" style={{fontSize:"0.65rem"}}>
+                            X{quantity}
+                        </span>
+                    }
+                </div>
+                <div>
+                    <span className="text-muted" style={{fontSize:"0.75rem"}}>
+                        {formatCurrency(item.price)}
+                    </span>
+                </div>
+            </div>
+            <div>
+                {formatCurrency(item.price*quantity)}
+            </div>
+            <Button variant="outline-danger" size="sm" onClick={()=>removeItem(item.id)}>
+                &times;
+            </Button>
         </Stack>
 
     )
